4ba73ca5507bc4766ab37a7dc9b322621cb44e1b
maxs110101 <maxs110101@gmail.com>
2022-10-23 22:48:02 +0200

    task 1, 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createImagesMarkup(imgArray) {
+  return imgArray
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join("");
+}
+
+galleryList.innerHTML = createImagesMarkup(galleryItems);
+
+// -----------------------------------------------------------
+
+galleryList.addEventListener("click", onOpenModal);
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery__item", {
+    captionDelay: 250,
+    overlayOpacity: 0.5,
+  });
+
+  document.addEventListener("keydown", onCloseModal);
+}
+
+function onCloseModal(evt) {
+  evt.preventDefault();
+
+  if (evt.code === "Escape") {
+    closed.simplelightbox;
+  }
+
+  document.removeEventListener("keydown", onCloseModal);
+}
